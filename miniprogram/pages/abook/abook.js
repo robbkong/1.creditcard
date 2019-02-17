c1408f6ee73f3fd2c10eb5392981fb4129971a3b
robbkong <821458745@qq.com>
2019-02-17 19:59:42 +0800

    if the info is null, not show the lists

--- miniprogram/pages/abook/abook.js
+++ miniprogram/pages/abook/abook.js
@@ -35,42 +35,48 @@ Page({
     this.show();
   },
   show: function () {
-    this.setData({ isnull: 1 });
     const db = wx.cloud.database()
     // 查询当前用户所有的 counters
     db.collection(this.data.database).where({
       _openid: this.data.openid
     }).get({
       success: res => {
-        var temp = res.data[0].info;
-        temp[0].id = res.data[0]._id;
-        for (let i = 1; i < res.data.length; i++) {
-          temp = temp.concat(res.data[i].info);
-          temp[i].id = res.data[i]._id;
+        console.log(res.data)
+        if (res.data[0] == undefined)
+        {
+          this.setData({ isnull: 1 });
         }
-        this.data.infos = temp;
-        console.log(temp)
-        console.log('[数据库] [查询记录] 成功: ', res.data)
-
-        // update the norateday and pay2dao
-        const length = this.data.infos.length;
-        for (let i = 0; i < length; ++i) {
-          this.data.infos[i].norateday = app.f_norateday(parseInt(this.data.infos[i].bill), parseInt(this.data.infos[i].pay));
-          this.data.infos[i].day2pay = app.f_day2pay(parseInt(this.data.infos[i].pay));
-        }
-
-        // 插入法排序，从大到小降序排序
-        for (let j = 1; j < length; ++j) {
-          var key = this.data.infos[j];
-          let i = j - 1;
-          while ((i >= 0) && (parseInt(this.data.infos[i].norateday) < parseInt(key.norateday))) {
-            this.data.infos[i + 1] = this.data.infos[i];
-            i = i - 1;
+        else
+        {
+          var temp = res.data[0].info;
+          temp[0].id = res.data[0]._id;
+          for (let i = 1; i < res.data.length; i++) {
+            temp = temp.concat(res.data[i].info);
+            temp[i].id = res.data[i]._id;
           }
-          this.data.infos[i + 1] = key;
+          this.data.infos = temp;
+          console.log('[数据库] [查询记录] 成功: ', res.data)
+
+          // update the norateday and pay2dao
+          const length = this.data.infos.length;
+          for (let i = 0; i < length; ++i) {
+            this.data.infos[i].norateday = app.f_norateday(parseInt(this.data.infos[i].bill), parseInt(this.data.infos[i].pay));
+            this.data.infos[i].day2pay = app.f_day2pay(parseInt(this.data.infos[i].pay));
+          }
+
+          // 插入法排序，从大到小降序排序
+          for (let j = 1; j < length; ++j) {
+            var key = this.data.infos[j];
+            let i = j - 1;
+            while ((i >= 0) && (parseInt(this.data.infos[i].norateday) < parseInt(key.norateday))) {
+              this.data.infos[i + 1] = this.data.infos[i];
+              i = i - 1;
+            }
+            this.data.infos[i + 1] = key;
+          }
+          this.setData({ isnull: 0 });
+          this.setData({ infos: this.data.infos });
         }
-        this.setData({ isnull: 0 });
-        this.setData({ infos: this.data.infos });
       },
       fail: err => {
         wx.showToast({
